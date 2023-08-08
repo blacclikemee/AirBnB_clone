a23190b91237bf0500ce3f7a1e2ed71e05766095
Avvvyy <uzochukwu.favour310@gmail.com>
2023-08-08 15:07:05 -0700

    Updating the __init__ tl use the kwargs parameters if available

--- models/base_model.py
+++ models/base_model.py
@@ -21,16 +21,24 @@ class BaseModel:
             None
         """
 
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
-        #Handling assigning of kwargs if kwargs is present
-        for key, value in kwargs.items():
-            if key == "created_at" or key == "updated_at":
-                setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-            else:
-                setattr(self, key, value)
 
     def __str__(self):
         """
